82f18060591ed8cb5a1391e16d3eb6266982a5b2
IInfinity <DevinDon@Foxmail.com>
2019-03-22 15:22:15 +0800

    feat(test): add all in one test method

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,7 +1,12 @@
 package com.example.demo;
 
+import com.example.demo.entity.UserEntity;
+import com.example.demo.mapper.UserMapper;
+
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +14,19 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class DemoApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+  @Autowired
+  private UserMapper userMapper;
+
+  @Test
+  public void allInOne() {
+    userMapper.deleteAll(); // Delete all users
+    Assert.assertEquals(0, userMapper.selectAll().size()); // Now, table `user` is empty
+    UserEntity user = new UserEntity("test", "test@test.com");
+    userMapper.insertOne(user); // Add new user
+    UserEntity checkUser = userMapper.selectAll().get(0);
+    Assert.assertNotNull(checkUser); // Now, table `user` should have 1 record
+    Assert.assertEquals(user.getName(), checkUser.getName()); // The name should be the same
+    Assert.assertEquals(user.getEmail(), checkUser.getEmail()); // Also the email
+  }
 
 }
