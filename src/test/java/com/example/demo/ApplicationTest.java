b3be9cd82138a8e4f21ec7d2e54b9eed67e97230
IInfinity <DevinDon@Foxmail.com>
2019-03-23 13:11:59 +0800

    refactor: ref class name

--- src/test/java/com/example/demo/ApplicationTest.java
+++ src/test/java/com/example/demo/ApplicationTest.java
@@ -12,7 +12,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class DemoApplicationTests {
+public class ApplicationTest {
 
   @Autowired
   private UserMapper userMapper;
